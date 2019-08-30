4edfef0b90fed68c115808c14d38f8c75e256f01
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-08-30 14:55:11 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -326,7 +326,7 @@ echo 'WEBMIN installed'
 echo 'OPENVPN server installed'
 echo 'The configuration file is available at /var/www/html/client.ovpn'
 echo 'Or http://your-ip/client.ovpn
-echo 'Download the .ovpn file and import it' "in" 'your OpenVPN client.'
+echo 'Download the .ovpn file and import it \in your OpenVPN client.'
 	exit 0
 
 }
