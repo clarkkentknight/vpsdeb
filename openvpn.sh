ab122f25a578d45496ee2f2007ba15595c3e749e
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-08-30 10:20:00 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -311,7 +311,7 @@ echo 'split-large-forms 0' >> /etc/privoxy/config
 echo 'keep-alive-timeout 5' >> /etc/privoxy/config
 echo 'tolerate-pipelining 1' >> /etc/privoxy/config
 echo 'socket-timeout 300' >> /etc/privoxy/config
-echo 'permit-access 0.0.0.0/0'"$IP" >> /etc/privoxy/config
+echo 'permit-access 0.0.0.0/0' "$IP" >> /etc/privoxy/config
 service privoxy restart
 	exit 0
 
