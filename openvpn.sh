ac78812071cfbe05548b716191288072ba4e4ef4
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-01 01:33:00 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -287,7 +287,8 @@ service stunnel4 restart
 
 function setbanner () {
 cat banner > /etc/issue.net
-sed -i 's@Banner none""@Banner /etc/banner"@g' /etc/ssh/sshd_config
+cp banner /etc/
+sed -i 's@#Banner none""@Banner /etc/banner"@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 service ssh restart
 service dropbear restart
