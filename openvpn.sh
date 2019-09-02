b00cadfb21f299168415faa18cc7d301d3ec9e0b
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-02 11:32:18 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -288,9 +288,10 @@ service webmin restart
 }
 
 function setall () {
-cat bann3r > /etc/issue.net
-cat banner > /etc/motd
-cp banner /etc/
+rm /etc/issue.net
+cat ~/openvpndeb/bann3r > /etc/issue.net
+cat ~/openvpndeb/banner > /etc/motd
+cp ~/openvpndeb/banner /etc/
 sed -i 's@#Banner[[:space:]]none@Banner /etc/banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 sed -i 's@ssl=1@ssl=0@g' /etc/webmin/miniserv.conf
