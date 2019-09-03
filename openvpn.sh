06592b67bc777c585fa94d5e8d41e0c4b2134f1f
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-03 22:58:15 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -309,14 +309,14 @@ function installQuestions () {
 	EXT_INT=$(cut -d' ' -f5 <(ip -4 route ls default))
 	IP=$(ip -4 addr ls $EXT_INT | head -2 | tail -1 | cut -d' ' -f6 | cut -d'/' -f1)
 	# If $IP is a private IP address, the server must be behind NAT
-	if echo "$IP" | grep -qE '^(10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|192\.168)'; then
-		echo ""
-		echo "It seems this server is behind NAT. What is its public IPv4 address or hostname?"
-		echo "We need it for the clients to connect to the server."
-		until [[ "$ENDPOINT" != "" ]]; do
-			read -rp "Public IPv4 address or hostname: " -e ENDPOINT
-		done
-	fi
+	#if echo "$IP" | grep -qE '^(10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|192\.168)'; then
+		#echo ""
+		#echo "It seems this server is behind NAT. What is its public IPv4 address or hostname?"
+		#echo "We need it for the clients to connect to the server."
+		#until [[ "$ENDPOINT" != "" ]]; do
+		#	read -rp "Public IPv4 address or hostname: " -e ENDPOINT
+		#done
+	#fi
 	echo ""
 	echo "What port do you want OpenVPN to listen to?"
 	echo "   1) Default: 1194"
